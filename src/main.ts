65524fac4381a1fcc1929a733b357914daa5fb30
18115710898 <lei006>
2022-07-06 10:52:00 +0800

    通过下面步骤，把 router ,vuex,弄到vite 创建的项目中去
    vit + vue3
    npm install -g @vue/cli
    vue create vue3-demo1
    
    还需要：
    1. 把create 创建的 src 复制到 vite 中
    2. 把  package.json 中的 dependencies修好
    3. npm i
    4. npm run dev 改错
    
    vite_demo.rar 是改后的
    
    /////////////////////////////////////////////////////////////////
    //  setup 语法糖插件 ： unplugin-auto-import
    
    yarn add unplugin-auto-import -D
    
    配置: vite.config.js (ts还有其它配置)
    
    import AutoImport from 'unplugin-auto-import/vite'
    // https://vitejs.dev/config/
    export default defineConfig({
      plugins: [
        vue(),
        AutoImport({
          imports:['vue', 'vue-router'] //自动导入
        })
      ]
    })

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
-import App from './App.vue'
-import 'ant-design-vue/dist/antd.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-
-
-let app = createApp(App)
-
-app.use(Antd);
-
-app.mount('#app')
+createApp(App).use(store).use(router).mount("#app");
