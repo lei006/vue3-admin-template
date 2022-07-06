7be5bf66cea25eb9b0273980794de4c5bb92629c
18115710898 <lei006>
2022-07-06 11:02:53 +0800

    更级一下

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+
+import Antd from 'ant-design-vue';
+import App from './App.vue'
+import 'ant-design-vue/dist/antd.css';
+
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+let app = createApp(App)
+
+
+app.use(store)
+app.use(router)
+app.use(Antd);
+
+
+
+
+app.mount("#app");
