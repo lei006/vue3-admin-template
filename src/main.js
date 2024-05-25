60397a066fefc62cf3ff12c254a6bc2846ff5d27
lei006_work <lei006@qq.com>
2024-05-25 17:50:49 +0800

    add pinia add elementplush

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import {createPinia} from 'pinia'
+const pinia = createPinia()
+
+
+console.log("meta.env", import.meta.env);
+
+const app = createApp(App);
+
+app.use(router)
+app.use(pinia)
+app.use(ElementPlus)
+
+app.mount('#app')
