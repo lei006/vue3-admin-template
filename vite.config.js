0cc3268b42bb89dd63c67a2b5f70cdfaa16b31e8
lei_home <lei006@qq.com>
2024-05-26 08:30:23 +0800

    0

--- vite.config.js
+++ vite.config.js
@@ -35,9 +35,11 @@ export default ({ command }) => {
       port: 3001,
       host: '0.0.0.0',
       open: true,
+      /*
       proxy: { // 代理配置
         '/dev': 'https://www.fastmock.site/mock/48cab8545e64d93ff9ba66a87ad04f6b/'
       },
+      */
     },
     build: {
       rollupOptions: {
