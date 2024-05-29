8c3ed82e853efbf51d2d007ee1eb511f3618b6d7
lei_home <lei006@qq.com>
2024-05-29 18:13:21 +0800

    加一点路由..

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import en from 'element-plus/dist/locale/en.mjs'
 
 const language = ref('zh-cn')
 const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
+let size = ref('small')
 
 const toggle = () => {
   language.value = language.value === 'zh-cn' ? 'en' : 'zh-cn'
@@ -17,11 +18,26 @@ const toggle = () => {
 
 <template>
 
-  <el-config-provider :locale="locale">
-    <el-button mb-2 @click="toggle">Switch Language</el-button>
+  <el-config-provider :locale="locale" :size="size">
+      <el-button mb-2 @click="toggle">Switch Language</el-button>
+      <el-button @click="size='small'">small</el-button>
+      <el-button @click="size='large'">large</el-button>
+      <el-button @click="size='default'">default</el-button>
+      <el-button>这是应用</el-button>
 
-    <el-table mb-1 :data="[]" />
+      <!--
+      <el-table mb-1 :data="[]" />
       <el-pagination :total="100" />
+      <div class="flex flex-wrap items-center mb-4">
+        <el-button size="large">Large</el-button>
+        <el-button>Default</el-button>
+        <el-button size="small">Small</el-button>
+        <el-button size="large" :icon="Search">Search</el-button>
+        <el-button :icon="Search">Search</el-button>
+        <el-button size="small" :icon="Search">Search</el-button>
+      </div>
+      -->
+      <router-view></router-view>
 
   </el-config-provider>
 
@@ -30,16 +46,5 @@ const toggle = () => {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      <el-button>Default</el-button>
+      <el-table mb-1 :data="[]" />
+      <el-pagination :total="100" />
+      <div class="flex flex-wrap items-center mb-4">
+        <el-button size="large">Large</el-button>
+        <el-button>Default</el-button>
+        <el-button size="small">Small</el-button>
+        <el-button size="large" >
+          <el-icon ><Edit /></el-icon>
+        </el-button>
+      </div>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+
+
 </script>
