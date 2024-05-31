69dcc693722463fc8e6c8321171ee318f6cb4d83
lei_home <lei006@qq.com>
2024-05-31 20:36:44 +0800

    0

---
+++ view/src/layout/mixin/ResizeHandler.js
@@ -0,0 +1,43 @@
+import AppStore from '@/pinia/app'
+let appStore = AppStore()
+
+const { body } = document
+const WIDTH = 992 // refer to Bootstrap's responsive design
+
+export default {
+  watch: {
+    $route(route) {
+      if (this.device === 'mobile' && this.sidebar.opened) {
+        appStore.closeSideBar({withoutAnimation:false})
+      }
+    }
+  },
+  beforeMount() {
+    window.addEventListener('resize', this.$_resizeHandler)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.$_resizeHandler)
+  },
+  mounted() {
+    const isMobile = this.$_isMobile()
+    if (isMobile) {
+      appStore.toggleDevice(mobile);
+      appStore.closeSideBar({ withoutAnimation: true });
+    }
+  },
+  methods: {
+    // use $_ for mixins properties
+    // https://vuejs.org/v2/style-guide/index.html#Private-property-names-essential
+    $_isMobile() {
+      const rect = body.getBoundingClientRect()
+      return rect.width - 1 < WIDTH
+    },
+    $_resizeHandler() {
+      if (!document.hidden) {
+        const isMobile = this.$_isMobile()
+        appStore.toggleDevice(isMobile ? 'mobile' : 'desktop');
+        appStore.closeSideBar({ withoutAnimation: true });
+      }
+    }
+  }
+}
