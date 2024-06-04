cc86612babeb15ac662ac745e069235991c59f3c
lei006_work <lei006@qq.com>
2024-06-04 10:17:56 +0800

    0

--- server/api/router/sys_operation_recode.go
+++
@@ -1,9 +0,0 @@
-package router
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func initOptionRecodeRouter(publicRouter *gin.RouterGroup, privateGroup *gin.RouterGroup) {
-
-}
--- server/api/router/sys_system.go
+++
@@ -1,9 +0,0 @@
-package router
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func initSystemRouter(publicRouter *gin.RouterGroup, privateGroup *gin.RouterGroup) {
-
-}
