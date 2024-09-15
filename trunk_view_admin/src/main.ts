c885775980184cf80e03cba0f32bde02cf71bc74
lei006_office <lei006_office@qq.com>
2024-09-15 10:30:02 +0800

    00

--- trunk_view_admin/src/main.ts
+++ trunk_view_admin/src/main.ts
@@ -30,3 +30,42 @@ getAuthRoutes().then(() => {
   app.mount('#app')
 })
 
+
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+  var o = {
+      "M+": this.getMonth() + 1, //月份 
+      "d+": this.getDate(), //日 
+      "h+": this.getHours(), //小时 
+      "m+": this.getMinutes(), //分 
+      "s+": this.getSeconds(), //秒 
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+      "S": this.getMilliseconds() //毫秒 
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+  if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
+
+
+
+function getQueryVariable(variable){
+    let hash = window.location.hash;
+    var arr_l = hash.split("?");
+    if (arr_l.length < 2) {
+        return false;
+    }
+    var query = arr_l[1];
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable)
+        {
+            return pair[1];
+        }
+    }
+
+    return(false);
+}
+
